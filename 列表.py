6fe076898db400a1edc606307c3fc1cd3261cfeb
aliceofalice <q1662053777@qq.com>
2020-08-02 22:24:57 +0800

    列表是个好东西

---
+++ 列表.py
@@ -0,0 +1,7 @@
+frinend = ['xie', 'li', 'yang', 'liu', 'ying']  # 这是一个数列
+
+print(frinend[0].title())  # 这里输出数列的第一个并大写（Python第一个是0)
+print(frinend[-1].title())  # 这里输出数列的倒一个并大写（Python倒一个是-1)
+print(frinend[-2].title())  # 这里输出数列的倒二个并大写（Python倒二个是-2，以此类推)
+fail = '今天是' + frinend[-2] + '先生的生日'
+print(fail)
