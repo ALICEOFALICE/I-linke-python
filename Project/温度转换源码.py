c4bdc6c577986394d36523f2220d69c25907a9ea
aliceofalice <q1662053777@qq.com>
2020-08-04 16:10:31 +0800

    （你以为我真跑了？其实我是重构了下文件结构）

---
+++ Project/温度转换源码.py
@@ -0,0 +1,11 @@
+def ccc(wendu):                                     #本行定义了一个叫ccc的函数，内置了温度转换
+    if wendu[-1] in ("F","f"):                      #调用if如果函数ccc返回的数值最后一位为（F，f），则进入if分支的算法
+        sheshidu =(eval(wendu[0:-1]) - 32) / 1.8    #eval将wendu的最后一位去掉后把其余部分转换为字符串，并且进行计算（wendu-32）/1.8
+        print(sheshidu)                             #输出变量sheshidu
+    elif wendu[-1] in ("C","c"):                    #如果返回数值为（C，c），则进入elif分支
+        huashedu = (eval(wendu[0:-1]) * 1.8) + 32   #eval将wendu的最后一位去掉后把其余部分转换为字符串,并进行计算（wendu*1.8) + 32
+        print (huashedu)                            #输出变量sheshidu
+    else:print("输入的数值错误")                     #如果是其他输入，返回错误信息
+
+wendu1 = input("请输入温度")                         #使用input函数把输入值赋予wendu1变量
+ccc(wendu1)                                         #调用ccc函数并设置输入值为wendu2
---
+++ Study/列表篇-修改,添加和删除元素.py
@@ -0,0 +1,45 @@
+print("这是一个实验整合项目，主要整合数列这个章节")
+# 本源码由桜火编写
+List = ["xie", "li", "yang", "liu", "rong", "rui"]
+print("列表默认状态", List)
+print("实验编码参考 \n 输出列表：1 \n 向列表末尾数值：2 \n 向列表指定位置添加数值:3 \n 向列表删除数值：4 \n 向列表弹出数值：5 \n 通过内容\
+向列表删除：6 ")
+ID = input("请选择实验编码")
+if ID in "1":
+    print("您选择了实验编码：1 \t 正在为您输入列表")
+    print(List)
+elif ID in "2":
+    print("您选择了实验编码：2 \t 请及时输入您要添加的数值")
+    LastAdd = input("请输入要添加到末尾的数值")
+    List.append(LastAdd)
+    print("正在为您添加列表并输出")
+    print(List)
+elif ID in "3":
+    print("您选择了实验编码：3 \t 请及时输入您要添加的位置与数值")
+    postion = input("请输入你需要的位置（从0开始）（-1代表倒数第二个）")
+    Add = input("请输入你要添加的数值")
+    List.insert(int(postion), Add)
+    print("正在为您添加列表并输出")
+    print(List)
+elif ID in "4":
+    print("您选择了实验编码：4 \t 请及时输入您要删除的位置")
+    Delpostion = input("您要删除的数值所在位置")
+    del List[int(Delpostion)]
+    print("正在为您删除列表并输出")
+    print(List)
+elif ID in "5":
+    print("您选择了实验编码：5 \t 请及时输入您要弹出的位置")
+    print("本次弹出将从列表中删除数值，并将被删除的数值赋予一个变量")
+    pop_up = input("请输入要弹出的数值")
+    pop_up_List = List.pop(int(pop_up))
+    print("正在为您弹出列表并输出")
+    print("弹出的数值", pop_up_List)
+    print("剩余列表", List)
+elif ID in "6":
+    connect = input("请输入列表中的内容")
+    List.remove(connect)
+    print("正在为您删除列表并输出（注意：Remove不会输出内容，和上面insert那些不一样）")
+    print(List)
+    print("不知道我那本书干啥，非要搞一个变量来存一下被删除的数值，那就输出出来吧", connect)
+else:
+    print("没其他的了，列表那张第二节讲完了")
